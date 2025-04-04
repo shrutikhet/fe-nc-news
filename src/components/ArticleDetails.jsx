c29b71690f70c819df4cda6ac72934cb865e7ec1
shrutikhetan0801 <shrutikhetan0801@gmail.com>
2025-04-04 12:17:51 +0100

    Error handling

--- src/components/ArticleDetails.jsx
+++ src/components/ArticleDetails.jsx
@@ -7,20 +7,25 @@ import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 import ThumbDownIcon from "@mui/icons-material/ThumbDown";
 import CommentBox from "./CommentBox";
 import { Tooltip } from "@mui/material";
+import Error from "./Error";
 
 function ArticleDetails() {
   const { article_id } = useParams();
   const [articleDetails, setArticleDetails] = useState({});
   const [addComment, setAddComment] = useState(false);
   const [optimisticVotes, setOptimisticVotes] = useState(0);
+  const [isError, setIsError] = useState(false);
+  const [errMsg, setErrMsg] = useState({});
 
   useEffect(() => {
     getArticleDetails(article_id)
       .then((data) => {
+        setIsError(false);
         setArticleDetails(data);
       })
       .catch((error) => {
-        console.log(error);
+        setIsError(true);
+        setErrMsg(error.response.data);
       });
   }, []);
 
@@ -59,44 +64,56 @@ function ArticleDetails() {
   }
 
   return (
-    <section className="article-item">
-      <img src={articleDetails.article_img_url} alt="soemthing else" />
-      <br />
-      <span id="article-title" className="title-bold">
-        {articleDetails.title}
-      </span>
-      <br />
-      <p id="article-body" className="color-title">
-        {articleDetails.body}
-      </p>
-      <br />
-      <span id="article-author" className="line-height">
-        {" "}
-        Author: {articleDetails.author}
-      </span>
-      <section className="item4">
-        <span id="article-comment">
-          Comment: {articleDetails.comment_count}
-        </span>
-        <br />
-        <span>{articleDetails.votes + optimisticVotes}</span>
-        <Tooltip title="Add Votes">
-          <ThumbUpIcon onClick={handleAddVote} />
-        </Tooltip>
-        {"  "}
+    <>
+      {!isError ? (
+        <>
+          {" "}
+          <section className="item3">
+            <img src={articleDetails.article_img_url} alt="soemthing else" />
+            <br />
+            <span id="article-title" className="title-bold">
+              {articleDetails.title}
+            </span>
+            <br />
+            <p id="article-body" className="color-title">
+              {articleDetails.body}
+            </p>
+            <br />
+            <span id="article-author" className="line-height">
+              {" "}
+              Author: {articleDetails.author}
+            </span>
 
-        <Tooltip title="decrese Votes">
-          <ThumbDownIcon onClick={handleMinusVote} />
-        </Tooltip>
-        <br />
-        <Tooltip title="Add Comment">
-          <CommentIcon onClick={handleClick} />
-        </Tooltip>
-        {addComment && <CommentBox setAddComment={setAddComment} />}
-      </section>
-      <br />
-      <Comments />
-    </section>
+            <br />
+            <Comments />
+          </section>
+          <section className="item4">
+            <span id="article-comment">
+              Comment: {articleDetails.comment_count}
+            </span>
+            <br />
+            <span>{articleDetails.votes + optimisticVotes}</span>
+            <Tooltip title="Add Votes">
+              <ThumbUpIcon onClick={handleAddVote} />
+            </Tooltip>
+            {"  "}
+
+            <Tooltip title="decrese Votes">
+              <ThumbDownIcon onClick={handleMinusVote} />
+            </Tooltip>
+            <br />
+            <Tooltip title="Add Comment">
+              <CommentIcon onClick={handleClick} />
+            </Tooltip>
+            {addComment && <CommentBox setAddComment={setAddComment} />}
+          </section>
+        </>
+      ) : (
+        <>
+          <Error errMsg={errMsg}/>
+        </>
+      )}
+    </>
   );
 }
 
